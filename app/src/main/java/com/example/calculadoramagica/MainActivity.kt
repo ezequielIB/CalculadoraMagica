a98838d1d678086c6e440ace175bc2177a177f6e
LeanNTech <crazywarrior43@gmail.com>
2023-10-16 16:27:46 -0300

    test comentario

--- app/src/main/java/com/example/calculadoramagica/MainActivity.kt
+++ app/src/main/java/com/example/calculadoramagica/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.calculadoramagica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/* hola esto es un comentario */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
